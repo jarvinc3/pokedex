645358dbaf79a5de5484695bc87611b1341403ec
jarvinc3 <jarvinct3@gmail.com>
2025-04-11 14:07:50 -0400

    feat: add Header and Footer components with basic layout and functionality

--- src/components/features/pokemons/PokemonCard.tsx
+++ src/components/features/pokemons/PokemonCard.tsx
@@ -1,7 +1,6 @@
-import { useFavoriteStore } from '@/store/favoriteStore';
 import { PokemonType } from '@/types/pokemon-detail';
 import { typeColors } from '@/types/pokemons-types';
-import { Heart, HeartOff, Zap } from 'lucide-react';
+import { Zap } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 interface PokemonCardProps {
@@ -11,33 +10,22 @@ interface PokemonCardProps {
 }
 
 export const PokemonCard = ({ name, image, type }: PokemonCardProps) => {
-  const { toggleFavorite, isFavorite } = useFavoriteStore();
   const primaryType = type?.[0];
   const primaryColor = typeColors[primaryType?.type.name as keyof typeof typeColors ?? 'normal']
   const gradientBackground = `linear-gradient(to bottom, transparent, ${primaryColor})`;
 
-
   return (
     <div
-      className={`rounded-2xl mt-10 shadow p-3 hover:shadow-lg transition relative duration-300 border h-72 border-slate-300`}
+      className={`rounded-2xl mt-14 shadow p-3 hover:shadow-lg transition relative duration-300 border h-72 border-slate-300`}
       style={{ background: gradientBackground }}
     >
-      <>
-        <button
-          onClick={() => toggleFavorite(name)}
-          className="absolute top-2 right-2 text-red-500 z-10"
-          title="Toggle favorite"
-        >
-          {isFavorite(name) ? <Heart className="fill-red-500" /> : <HeartOff />}
-        </button>
-        <div className="absolute inset-0 z-0 pointer-events-none">
-          <img
-            src="/img/pokeball-bg-2.png"
-            alt="Pokeball"
-            className="w-full h-full object-contain opacity-10"
-          />
-        </div>
-      </>
+      <div className="absolute inset-0 z-0 pointer-events-none">
+        <img
+          src="/img/pokeball-bg-2.png"
+          alt="Pokeball"
+          className="w-full h-full object-contain opacity-10"
+        />
+      </div>
 
       <Link to={`/pokemon/${name}`} className="relative h-full flex flex-col justify-end">
         <img src={image} alt={name} className="size-auto mx-auto z-50" />
@@ -65,3 +53,26 @@ export const PokemonCard = ({ name, image, type }: PokemonCardProps) => {
   );
 };
 
+// export const PokemonCardSkeleton = ({ name, image, type }: PokemonCardProps) => {
+//   return (
+//     <div className="card">
+//       <img src={image} alt={name} className="size-auto mx-auto z-50" />
+//       <h2 className="text-center mb-3 capitalize font-semibold text-xl mt-1 text-white drop-shadow shadow-black">
+//         • {name} •
+//       </h2>
+
+//       <div className="flex justify-center items-end gap-1 w-full">
+//         {type?.map((type, index) => (
+//           <span
+//             key={index}
+//             className="text-sm font-medium text-white drop-shadow shadow-black w-full py-1 rounded-md flex justify-center items-center gap-1 border border-slate-900/20"
+//             style={{ backgroundColor: typeColors[type.type.name as keyof typeof typeColors] }}
+//           >
+//             <img src={`/icons/${type.type.name}-type.ico`} className="w-4 h-4" alt="" />
+//             {type.type.name}
+//           </span>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router-dom"
+import { Footer } from "./footer"
+import { Header } from "./header"
 
 
 export const MainLayout = () => {
    return (
       <body className="min-h-screen flex justify-between flex-col">
-         <header className="bg-gray-800 text-white p-4">
-            <h1 className="text-2xl">Pokemon App</h1>
+         <header className="p-4">
+            <Header />
          </header>
          <main className="p-4 h-full">
             <Outlet />
          </main>
+         <footer className="rounded-lg mx-16 mb-10 border-2 border-gray-500 p-4 flex justify-between items-center">
+            <Footer />
+         </footer>
       </body>
    )
 }
